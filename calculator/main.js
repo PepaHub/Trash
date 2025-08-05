2ebe4e247d76de49b2824a35d44eb4900ed792e6
PepaHub <nickminaylov@gmail.com>
2025-08-05 15:34:53 +0300

    теперь работает сброс

--- calculator/main.js
+++ calculator/main.js
@@ -3,3 +3,16 @@ document.querySelector('.result').style.display = 'none';
 document.getElementById('calculate-btn').addEventListener('click', function () {
     document.querySelector('.result').style.display = 'block';
 });
+
+// Скрываем результат при изменении чисел или операции
+document.getElementById('first-number-select').addEventListener('change', function() {
+    document.querySelector('.result').style.display = 'none';
+});
+
+document.getElementById('second-number-select').addEventListener('change', function() {
+    document.querySelector('.result').style.display = 'none';
+});
+
+document.getElementById('operation-select').addEventListener('change', function() {
+    document.querySelector('.result').style.display = 'none';
+});
