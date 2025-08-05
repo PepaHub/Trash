aa0b9e887c892ca467046405f23193599a866dc5
PepaHub <nickminaylov@gmail.com>
2025-08-05 15:15:49 +0300

    first child

---
+++ calculator/main.js
@@ -0,0 +1,5 @@
+document.querySelector('.result').style.display = 'none';
+
+document.getElementById('calculate-btn').addEventListener('click', function () {
+    document.querySelector('.result').style.display = 'block';
+});
